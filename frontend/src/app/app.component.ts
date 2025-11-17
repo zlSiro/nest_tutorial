ca05ae1b55950d6b8663f6b50db454a06325b9e0
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-11-17 18:06:55 -0300

    feat(app): actualizar título de la aplicación

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styles: [],
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Tienda - Admin';
 }
