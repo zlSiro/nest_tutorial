211cd47c1d9f4e09404ba744f7e68ec78a2a76e9
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-11-17 18:07:18 -0300

    feat(routes): agregar rutas para redireccionar a la lista de usuarios

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'users', component: UsersListComponent },
+];
